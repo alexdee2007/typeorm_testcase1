25db475bada3b6523e6558fe1763a5648442b3c8
o.diachenko.2005 <o.diachenko.2005@in.np.gov.ua>
2020-10-19 22:11:18 +0300

    wip

--- src/dbconfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  user: 'ers',
-  password: 'ers',
-  connectString: '101.8.1.239/orcl'
-};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import 'reflect-metadata';
-import { createConnection, getConnection, getRepository } from 'typeorm';
+import {
+  createConnection,
+  getConnection,
+  getRepository,
+  Repository,
+  FindOptionsUtils,
+  SelectQueryBuilder,
+  EntityManager,
+} from 'typeorm';
 import { Timer } from './timer';
 import { Address } from './entity/Address';
 import { Case } from './entity/Case';
@@ -26,13 +34,46 @@ import { Thing } from './entity/Thing';
 import { Upload } from './entity/Upload';
 import { User } from './entity/User';
 import { Wanted } from './entity/Wanted';
+import { resolve } from 'path';
 
-const oracledb = require('oracledb');
-const dbConfig = require('./dbconfig.js');
+/*
+(SelectQueryBuilder.prototype as any).loadAllRelationInclude = function () {
+  console.log('loadAllRelationInclude-----------');
+  console.log(this);
+  return this;
+};
+
+const applyOptionsToQueryBuilder = FindOptionsUtils.applyOptionsToQueryBuilder.bind(
+  FindOptionsUtils,
+);
+FindOptionsUtils.applyOptionsToQueryBuilder = function (qb, options: any) {
+  if (options.include === 'all') {
+    (qb as any).loadAllRelationInclude();
+  }
+
+  return applyOptionsToQueryBuilder(qb, options);
+};
+*/
+
+const EntityManagerPrototypeFind = EntityManager.prototype.find;
+
+EntityManager.prototype.find = async function (
+  entityClass,
+  optionsOrConditions,
+) {
+  if (optionsOrConditions.include && optionsOrConditions.include === 'all') {
+    console.log('include all');
+  }
+  const ret = await EntityManagerPrototypeFind.call(
+    this,
+    entityClass,
+    optionsOrConditions,
+  );
+  console.log(ret);
+  return ret;
+};
 
 (async () => {
-  const connectionOra = await oracledb.getConnection(dbConfig);
-
   const connection = await createConnection();
 
   try {
@@ -40,7 +81,9 @@ const dbConfig = require('./dbconfig.js');
       '\n\n############################################## Query the DB',
     );
 
-    const query = getRepository(Ers)
+    const ersRepository = getRepository(Ers);
+
+    const query = ersRepository
       .createQueryBuilder('Ers')
       // .leftJoinAndSelect('Ers.User', 'Ers_User')
       .leftJoinAndSelect('Ers.Materials', 'Ers_Materials')
@@ -58,44 +101,46 @@ const dbConfig = require('./dbconfig.js');
       .leftJoinAndSelect('Ers_Things.Uploads', 'Ers_Things_Uploads')
       .leftJoinAndSelect('Ers_Phones.Uploads', 'Ers_Phones_Uploads')
       .leftJoinAndSelect('Ers_Photos.Uploads', 'Ers_Photos_Uploads')
-      .leftJoinAndSelect('Ers_Features.Uploads', 'Ers_Features_Uploads');
-    // .select([
-    //   'Ers.id',
-    //   'Ers_User.id',
-    //   'Ers_Materials.id',
-    //   'Ers_Materials_Uploads.id',
-    //   'Ers_Materials_PrimaryMaterials.id',
-    //   'Ers_Materials_Signers.id',
-    //   'Ers_Descriptions.id',
-    //   'Ers_Features.id',
-    //   'Ers_Phones.id',
-    //   'Ers_Photos.id',
-    //   'Ers_Things.id',
-    //   'Ers_Things_Uploads.id',
-    //   'Ers_Photos_Uploads.id',
-    //   'Ers_Features.id',
-    // ]);
-    // .where('Ers.id = 105');
+      .leftJoinAndSelect('Ers_Features.Uploads', 'Ers_Features_Uploads')
+      // .select([
+      //   'Ers.id',
+      //   'Ers_User.id',
+      //   'Ers_Materials.id',
+      //   'Ers_Materials_Uploads.id',
+      //   'Ers_Materials_PrimaryMaterials.id',
+      //   'Ers_Materials_Signers.id',
+      //   'Ers_Descriptions.id',
+      //   'Ers_Features.id',
+      //   'Ers_Phones.id',
+      //   'Ers_Photos.id',
+      //   'Ers_Things.id',
+      //   'Ers_Things_Uploads.id',
+      //   'Ers_Photos_Uploads.id',
+      //   'Ers_Features.id',
+      // ]);
+      .where('Ers.id = 105');
 
-    console.log('query: ', query.getSql());
+    // console.log('query: ', query.getSql());
 
-    for (let nTest = 0; nTest < 5; nTest++) {
-      console.log(`\n>>>>>>>> Test n.${nTest}`);
-      const t0 = new Timer('Raw query');
-      const raw = await connection.query(query.getSql());
-      // console.log(raw);
-      t0.stop();
-      console.log('N. of rows returned: ', raw.length);
-      const t1 = new Timer('Query with hydration');
-      const res = await query.getMany();
-      //const result =
-      //await connectionOra.execute(query.getSql());
-      t1.stop();
-    }
+    //for (let nTest = 0; nTest < 5; nTest++) {
+    // console.log(`\n>>>>>>>> Test n.${nTest}`);
+    // const t0 = new Timer('Raw query');
+    // const raw = await connection.query(query.getSql());
+    // console.log(raw);
+    // t0.stop();
+    // console.log('N. of rows returned: ', raw.length);
+    const t1 = new Timer('Query with hydration');
+    const res = await (ersRepository.find as any)({
+      where: { id: '105' },
+      include: 'all',
+    });
+    console.log('N. of Entities returned: ', res.length);
+    // console.log(res);
+    t1.stop();
+    //}
   } catch (e) {
     console.error('Exception: ', e);
   }
   await connection.close();
-  await connectionOra.close();
   console.log('DONE!');
 })();
